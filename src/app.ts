4ae7274056e2da212b7cb4b5cab1017d616d3c95
Francisco Cortez <franciscocortezdev@gmail.com>
2022-12-08 15:45:33 -0600

    Database conexion

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import express, { Application } from "express"
+import dotenv from "dotenv"
+dotenv.config()
 import { router } from "./routes"
 import cors from "cors"
+import mongoose from "mongoose"
 
 const app: Application = express()
+const PORT = process.env.PORT || 3000
+const DB_URI = <string>process.env.DB_URI
+
+
+mongoose
+  .connect(DB_URI)
+  .then(() => console.log("Database connected"))
+  .catch((err) => {
+    console.log(err.message)
+  })
 
 app.use(cors())
 app.use(express.json())
 app.use(router)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server started")
 })
