83aa02d63ba670b06044259acb2b31de0428b675
Francisco Cortez <franciscocortezdev@gmail.com>
2022-12-08 16:15:38 -0600

    DB connection refactorized

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,18 @@
 import express, { Application } from "express"
-import dotenv from "dotenv"
-dotenv.config()
 import { router } from "./routes"
 import cors from "cors"
-import mongoose from "mongoose"
+import dbConnect from "./config/mongoConnection"
 
 const app: Application = express()
 const PORT = process.env.PORT || 3000
-const DB_URI = <string>process.env.DB_URI
-
-
-mongoose
-  .connect(DB_URI)
-  .then(() => console.log("Database connected"))
-  .catch((err) => {
-    console.log(err.message)
-  })
 
 app.use(cors())
 app.use(express.json())
 app.use(router)
+
+dbConnect()
+  .then((result) => console.log("Database conected"))
+  .catch((error) => console.log("Database connection error"))
 
 app.listen(PORT, () => {
   console.log("Server started")
---
+++ src/config/mongoConnection.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv"
+dotenv.config()
+import mongoose from "mongoose"
+
+const dbConnect = async () => {
+  const DB_URI = <string>process.env.DB_URI
+  await mongoose.connect(DB_URI) 
+}
+
+
+export default dbConnect
