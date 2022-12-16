6ef303850cc65f9e94d1e6d13fb2659afeb315f7
Francisco Cortez <franciscocortezdev@gmail.com>
2022-12-15 20:31:04 -0600

    User login servide

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -5,3 +5,10 @@ export const userRegister = async (user: UserInterface) => {
   const responseInsert = await userModel.create(user);
   return responseInsert;
 };
+
+
+export const userLogin = async (id: string) => {
+  const responseItem = await userModel.findOne({_id: id})
+  return responseItem
+}
+
