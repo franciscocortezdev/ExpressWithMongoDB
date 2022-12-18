74056d3b3fded88c013f1cda70506bc0907c8dec
Francisco Cortez <franciscocortezdev@gmail.com>
2022-12-18 15:32:07 -0600

    JWT methods

---
+++ src/utils/handleJWT.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv"
+dotenv.config()
+import { Secret, sign, verify } from "jsonwebtoken"
+import { AuthInterface } from "../interfaces/authInterface"
+
+const secretJWT = process.env.JWT_SECRET
+
+export const generateToken = (credentials: AuthInterface) => {
+  const jwt = sign(credentials, secretJWT as Secret, { expiresIn: "1h" })
+
+  return jwt
+}
+
+export const compareToken = (token: string) => {
+  const response = verify(token, secretJWT as Secret)
+  return response
+}
