49af17c01dddf20b0fe45e36be7870078bcd50a4
Francisco Cortez <franciscocortezdev@gmail.com>
2022-12-19 20:14:08 -0600

    Sesion middleware

---
+++ src/middleware/session.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Response } from "express"
+import { reqExtend } from "../interfaces/reqExtend"
+import { handleError } from "../utils/handleError"
+import { compareToken } from "../utils/handleJWT"
+import { JwtPayload } from "jsonwebtoken";
+
+
+export const authSession = (
+  req: reqExtend,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const jwtUser = req.headers.authorization
+    if (!jwtUser) return handleError(res, "ERROR_JWT_NOT_FOUND")
+    const jwt = jwtUser.split(" ").pop()
+    const jwtCorrect = compareToken(`${jwt}`)
+    req.user = jwtCorrect as JwtPayload 
+    next()
+  } catch (error) {
+    handleError(res, "SESSION_NO_VALID")
+  }
+}
