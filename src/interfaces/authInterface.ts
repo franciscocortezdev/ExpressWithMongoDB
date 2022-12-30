d7093ed7d8175a6c9b361392b9a662d1caed4534
Francisco Cortez <franciscocortezdev@gmail.com>
2022-12-29 18:58:38 -0600

    Interface for login params

--- src/interfaces/authInterface.ts
+++ src/interfaces/authInterface.ts
@@ -5,3 +5,15 @@ export interface AuthInterface{
   password: string;
   _id: mongoose.Types.ObjectId
 }
+
+export interface loginParams{
+  errors: [
+    {
+      location: string,
+      msg: string,
+      param: string
+    }
+  ]
+}
+
+
